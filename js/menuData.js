775f4de1e743f952b00bb38563a0ece6eb7943f2
YangDiaa <736208296@qq.com>
2020-04-07 21:37:53 +0800

    功能实现

--- js/menuData.js
+++ js/menuData.js
@@ -109,13 +109,19 @@ np.menuData = [
                 title: '使用 Bing 搜索...',
                 shortcut: 'Ctrl+E',
                 enabled: true,
-                handler: function() { console.log('使用 Bing 搜索 menu clicked!'); }
+                handler: function() { 
+                    $editor.bingSearch(); 
+                }
             },
             {
                 title: '查找(F)...',
                 shortcut: 'Ctrl+F',
                 enabled: false,
-                handler: function() { console.log('查找(F) menu clicked!'); }
+                handler: function() {
+                    $dlgSearch.show(function(srch) {
+                        $editor.search(srch);
+                    });
+                }
             },
             {
                 title: '查找下一个(N)',
@@ -127,13 +133,33 @@ np.menuData = [
                 title: '替换(R)...',
                 shortcut: 'Ctrl+H',
                 enabled: true,
-                handler: function() { console.log('替换(R) menu clicked!'); }
+                handler: function() {
+                    $dlgReplace.show({
+                        searchHandle: function(e) {
+                            $editor.search(e);
+                        },
+                        replaceHandle: function(e) {
+                            $editor.replace(e);
+                        },
+                        replaceAllHandle: function(e) {
+                            $editor.replaceAll(e);
+                        }
+                    });
+                }
             },
             {
                 title: '转到(G)...',
                 shortcut: 'Ctrl+G',
                 enabled: true,
-                handler: function() { console.log('转到(G) menu clicked!'); }
+                handler: function() {
+                    $dlgGoto.show({
+                        lineNum: $editor.getRow(),
+                        totalLine: $editor.getTotalLn(),
+                        gotoHandler: function(lineNum) {
+                            $editor.gotoLn(lineNum);
+                        }
+                    });
+                }
             },
             {
                 title: 'hr',
@@ -145,13 +171,17 @@ np.menuData = [
                 title: '全选(A)',
                 shortcut: 'Ctrl+A',
                 enabled: true,
-                handler: function() { console.log('全选(A) menu clicked!'); }
+                handler: function() {
+                    $editor.selectAll();
+                }
             },
             {
                 title: '时间/日期(D)',
                 shortcut: 'F5',
                 enabled: true,
-                handler: function() { console.log('时间/日期(D) menu clicked!'); }
+                handler: function() {
+                    $editor.insertDataTime();
+                }
             },
         ],
         width: '218px',
@@ -164,13 +194,39 @@ np.menuData = [
                 title: '自动换行(W)',
                 shortcut: '',
                 enabled: true,
-                handler: function() { console.log('自动换行(W) menu clicked!'); }
+                handler: function() {
+                    np.bWrap = !np.bWrap;
+
+                    if(np.bWrap) {
+                        $statusBar.check(false);
+                        $editor.resize(false);
+                        $menuBar.enabled(3, 0, false);   // [查看]-[状态栏]菜单禁用
+                        $menuBar.enabled(1, 9, false);   // [编辑]-[转到]菜单禁用
+                    } 
+                    else {
+                        $statusBar.check(np.bShowStatusBar);
+                        $editor.resize(np.bShowStatusBar);
+                        $menuBar.enabled(3, 0, true);     // [查看]-[状态栏]菜单启用
+                        $menuBar.enabled(1, 9, true);     // [编辑]-[转到]菜单启用
+                        $menuBar.checked(3, 0, np.bShowStatusBar);
+                    }
+          
+                    $menuBar.checked(2, 0, np.bWrap);
+                    $editor.setWrap(np.bWrap);
+                }
             },
             {
                 title: '字体(F)...',
                 shortcut: '',
                 enabled: true,
-                handler: function() { console.log('字体(F) menu clicked!'); }
+                handler: function() {
+                    $dlgFont.show({
+                        family: np.fontFamily,
+                        style: np.fontStyle,
+                        size: np.fontSize,
+                        okHandler: np.fontHandler
+                    });
+                }
             }
         ],
         width: '156px',
@@ -187,6 +243,7 @@ np.menuData = [
                     np.bShowStatusBar = !np.bShowStatusBar;
                     $statusBar.check(np.bShowStatusBar);
                     $menuBar.checked(3, 0, np.bShowStatusBar);
+                    $editor.resize(np.bShowStatusBar);
                 }
             }
         ],
@@ -200,13 +257,17 @@ np.menuData = [
                 title: '查看帮助(H)',
                 shortcut: '',
                 enabled: true,
-                handler: function() { console.log('查看帮助(H) menu clicked!'); }
+                handler: function() {
+                    window.open('https://cn.bing.com/search?q=获取有关+windows+10+中的记事本的帮助', '_blank');
+                }
             },
             {
                 title: '关于记事本(A)',
                 shortcut: '',
                 enabled: true,
-                handler: function() { $dlgAbout.show(); }
+                handler: function() { 
+                    $dlgAbout.show(); 
+                }
             },
         ],
         width: '166px',
